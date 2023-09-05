8450c549e421ff61141f885cfae5355ce8f5d400
Ayush Agrawal <ayushagrawal1@districtd.co>
2023-09-05 22:31:55 +0530

    CI : secure password save in DB

--- controllers/signUp.js
+++ controllers/signUp.js
@@ -1,20 +1,25 @@
 const db = require('../models/index');
+const bcrypt = require('bcryptjs');
 
 module.exports = {
     signUp : async (req, res, next) => {
         try {
+            // Password Secure
+            const salt = bcrypt.genSaltSync(10);
+            const securePassword = bcrypt.hashSync(req.body.password, salt);
+
             // Data preparation
             const data = {
                 first_name: req.body.first_name,
                 last_name: req.body.last_name,
                 username: req.body.username,
                 email: req.body.email,
-                password: req.body.password,
+                password: securePassword,
                 is_admin: req.body.is_admin,
             }
 
             // Fetch Data.
-            let userData = await db.users.findOne({
+            const userData = await db.users.findOne({
                 where : {
                     [db.Sequelize.Op.or] : {
                         username: req.body.username || null,
