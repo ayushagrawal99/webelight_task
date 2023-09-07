5d9bdb7af2437be2a1f6ac88034e3f25882179d9
Ayush Agrawal <ayushagrawal1@districtd.co>
2023-09-07 11:27:12 +0530

    CI : apply filter

--- controllers/allItems.js
+++ controllers/allItems.js
@@ -3,10 +3,23 @@ const db = require('../models/index');
 module.exports = {
     allItem : async (req, res, next) => {
         try {
+            // Filter
+            let where = {};
+            if(req.query.category){
+                where.category = req.query.category;
+            }
+            if(req.query.name){
+                where.name = req.query.name;
+            }
+            if(req.query.price){
+                where.price = req.query.price;
+            }
+
             // Fetch Data.
             let itemData = await db.items.findAll({
                 offset : parseInt(req.query.offset) || 0,
                 limit : parseInt(req.query.limit) || 5,
+                where : where,
                 raw : true
             });
 
